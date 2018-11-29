ee1c6ed56eb5666c2466680c187f992275d2d524
Cam <cam@camshepherd.com>
2018-11-29 22:40:46 +0000

    Migrations

--- HouseLemmingv3/Data/Migrations/20181129223808_Initial.Designer.cs
+++ HouseLemmingv3/Data/Migrations/20181129223808_Initial.Designer.cs
@@ -4,14 +4,16 @@ using HouseLemmingv3.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HouseLemmingv3.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181129223808_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
