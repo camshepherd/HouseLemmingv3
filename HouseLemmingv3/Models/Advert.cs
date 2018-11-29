24206d39ee379155339d3fce289c2bbfa6bf8755
Cam <cam@camshepherd.com>
2018-11-29 22:36:53 +0000

    Initial Models

---
+++ HouseLemmingv3/Models/Advert.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace HouseLemmingv3.Models
+{
+    public class Advert
+    {
+            [Key]
+            [Required] public int AdvertId { get; set; }
+
+            public virtual ICollection<Request> Requests { get; set; }
+
+            [Required] public string UserId { get; set; }
+
+            [Required]
+            [MaxLength(140)]
+            [MinLength(4)]
+            public string DescShort { get; set; }
+
+            [Required]
+            [MaxLength(10000)]
+            [MinLength(20)]
+            public string DescLong { get; set; }
+
+
+            [Required]
+            [Range(0, int.MaxValue)]
+            public int NumToilets { get; set; }
+
+
+            [Range(1, int.MaxValue, ErrorMessage = "There has to be as least one room")]
+            [Required]
+            public int NumRooms { get; set; }
+
+            [Required] [DataType(DataType.Currency)] public float PriceMonthly { get; set; }
+            [Required] [DataType(DataType.Currency)] public float Deposit { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime StartDate { get; set; }
+            [Required] [DataType(DataType.Date)] public DateTime EndDate { get; set; }
+
+
+            // 0 = closed (unconfirmed / declined approval / room already taken / removed by owner) and hidden, 1 = open (available to rent) and visible 
+            [Required]
+            [Range(0, 1)]
+            [DefaultValue(0)]
+            public int Status { get; set; }
+
+            // Do not want regex on the phone number as many phone numbers do not conform to standard format
+            [Required]
+            [RegularExpression(@"^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$")]
+            public string ContactNumber { get; set; }
+
+            //Pretty sure that regex on email should be fine
+            [Required]
+            [RegularExpression(@"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                               + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                               + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                               + @"([a-zA-Z0-9]+[\w-]+\.)+[a-zA-Z]{1}[a-zA-Z0-9-]{1,23})$")]
+            //[RegularExpression("^\w + ([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")]
+            public string ContactEmail { get; set; }
+
+
+
+            [Required]
+            [MaxLength(100)]
+            [MinLength(5)]
+            public string AddrLine1 { get; set; }
+
+            [MaxLength(100)]
+            [MinLength(4)]
+            public string AddrLine2 { get; set; }
+
+            [Required]
+            [MaxLength(60)]
+            [MinLength(4)]
+            public string AddrCity { get; set; }
+
+            [MaxLength(30)]
+            [MinLength(4)]
+            public string AddrCounty { get; set; }
+
+            [Required]
+            [RegularExpression("^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$")]
+            public string AddrPostCode { get; set; }
+    }
+}
---
+++ HouseLemmingv3/Models/Request.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseLemmingv3.Models
+{
+    public class Request
+    {
+        [Key]
+        [Required]
+        public int RequestId { get; set; }
+
+        [Required] public int SubjectId { get; set; }
+
+        [MaxLength(140)] public string Feedback { get; set; }
+
+        [Required]
+        [Range(0, 2)]
+        //Let 0=declined,1=unaddressed,2=accepted
+        [DefaultValue(1)]
+        public int Approval { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)] public DateTime DateCreation { get; set; }
+
+        [DataType(DataType.Date)] public DateTime DateResponse { get; set; }
+    }
+}
