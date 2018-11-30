5309d9343cb3cf097a53c013f74ce9e354c2a99e
Cam <cam@camshepherd.com>
2018-11-30 11:19:09 +0000

    Add Image

---
+++ HouseLemmingv3/Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseLemmingv3.Models
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string FileName { get; set; }
+
+        [Required] public Guid AdvertId { get; set; }
+        public Advert Advert { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
