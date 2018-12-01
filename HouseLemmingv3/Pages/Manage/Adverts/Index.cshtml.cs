0e6ae95dd2028b775499a822146a40b21d969468
Cam <cam@camshepherd.com>
2018-12-01 19:01:11 +0000

    Only show owned houses for Landlords

--- HouseLemmingv3/Pages/Manage/Adverts/Index.cshtml.cs
+++ HouseLemmingv3/Pages/Manage/Adverts/Index.cshtml.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HouseLemmingv3.Areas.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HouseLemmingv3.Data;
 using HouseLemmingv3.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace HouseLemmingv3.Pages.Manage
 {
@@ -17,14 +20,27 @@ namespace HouseLemmingv3.Pages.Manage
         public IndexModel(HouseLemmingv3.Data.ApplicationDbContext context)
         {
             _context = context;
+            UserManager = _context.GetService<UserManager<ApplicationUser>>();
         }
 
+        private UserManager<ApplicationUser> UserManager;
+        private RoleManager<IdentityRole> RoleManager;
         public IList<Advert> Advert { get;set; }
+        
 
         public async Task OnGetAsync()
         {
-            Advert = await _context.Adverts
-                .Include(a => a.ApplicationUser).ToListAsync();
+            Guid UserId = UserManager.GetUserAsync(HttpContext.User).Result.Id;
+            IList<string> Role = UserManager.GetRolesAsync(UserManager.GetUserAsync(HttpContext.User).Result).Result;
+            if (Role.Contains("Landlord"))
+            {
+                Advert = await _context.Adverts
+                    .Include(a => a.ApplicationUser).Where(u => u.ApplicationUserId == UserId).ToListAsync();
+            }
+            else
+            {
+                Advert = await _context.Adverts.Include(a => a.ApplicationUser).ToListAsync();
+            }
         }
     }
 }
